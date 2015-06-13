e8fbe7db7b8c0f6c816a57bbb2586ed7388b66cb
zhangjie <luoyetx@gmail.com>
2015-06-13 16:55:57 +0800

    change double to float

--- writeBinary.m
+++ writeBinary.m
@@ -12,6 +12,7 @@ landmarkSize = regModel.model.nfids;
 landmarkDim = regModel.model.D;
 M = size(regModel.regs(1).regInfo{1, 1}.thrs, 2);
 featureSize = regModel.regs(1).ftrPos.F;
+N = size(regModel.pGtN,1);
 fwrite(fd, T, 'int32');
 fwrite(fd, regSize, 'int32');
 fwrite(fd, fernSize, 'int32');
@@ -19,16 +20,26 @@ fwrite(fd, landmarkSize, 'int32');
 fwrite(fd, landmarkDim, 'int32');
 fwrite(fd, M, 'int32');
 fwrite(fd, featureSize, 'int32');
+fwrite(fd, N, 'int32');
+
+% 写入训练时的形状
+for i=1:N
+    for j=1:landmarkDim
+        fwrite(fd, regModel.pGtN(i, j), 'float32');
+    end
+end
 
 % 写入模型参数
 for i=1:T
     fprintf('正在写入第 %i 级回归器参数\n', i)
     % 先写入特征池 id1, id2, t1
+    % 选择的特征编号，分成两行，我们令编号从 0 开始
     xs = regModel.regs(i).ftrPos.xs;
     for j=1:featureSize
-        fwrite(fd, xs(j, 1), 'int32');
-        fwrite(fd, xs(j, 2), 'int32');
-        fwrite(fd, xs(j, 3), 'double');
+        % 关键点编号从 0 开始
+        fwrite(fd, xs(j, 1)-1, 'int32');
+        fwrite(fd, xs(j, 2)-1, 'int32');
+        fwrite(fd, xs(j, 3), 'float32');
     end
     % 写入随机厥参数 15x3 = 15 + 15 + 15
     for r=1:regSize
@@ -43,12 +54,12 @@ for i=1:T
             end
             % 特征值阈值
             for m=1:M
-                fwrite(fd, reg.thrs(1, m), 'double');
+                fwrite(fd, reg.thrs(1, m), 'float32');
             end
             % 随机厥叶节点的形状增量
             for k=1:2^M
                 for p=1:landmarkDim
-                    fwrite(fd, reg.ysFern(k, p), 'double');
+                    fwrite(fd, reg.ysFern(k, p), 'float32');
                 end
             end
         end
